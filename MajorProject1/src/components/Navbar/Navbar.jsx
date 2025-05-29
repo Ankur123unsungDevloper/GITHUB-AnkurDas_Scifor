cc354865588bc9a3f74e6f9990b54a6242a34b82
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-05-30 00:59:33 +0530

    new file:   MajorProject1/.gitignore 	new file:   MajorProject1/README.md 	new file:   MajorProject1/eslint.config.js 	new file:   MajorProject1/index.html 	new file:   MajorProject1/package-lock.json 	new file:   MajorProject1/package.json 	new file:   MajorProject1/public/vite.svg 	new file:   MajorProject1/src/App.jsx 	new file:   MajorProject1/src/assets/logo.png 	new file:   MajorProject1/src/components/Navbar/Navbar.jsx 	new file:   MajorProject1/src/index.css 	new file:   MajorProject1/src/main.jsx 	new file:   MajorProject1/vite.config.js

---
+++ MajorProject1/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from "../../assets/logo.png"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <a href="#">
+              <img src={Logo} alt="Logo" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
