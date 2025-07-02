5267f1bff74bac52a75ae00570f181ea65a44eda
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-03 00:40:41 +0530

    modified:   Projects/ClassProject/src/components/Navbar.jsx 	modified:   Projects/ClassProject/src/index.css

--- Projects/ClassProject/src/components/Navbar.jsx
+++ Projects/ClassProject/src/components/Navbar.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RiComputerLine } from "react-icons/ri";
 import { IoTabletPortraitOutline } from "react-icons/io5";
 import { LiaMobileSolid } from "react-icons/lia";
+import { IoMdReturnRight } from "react-icons/io";
 
 function Navbar() {
+  const [active, setActive] = useState('desktop');
+
   return (
     <header className="nav">
       <div className="nav-left">
-        {/* <img src="../assets/logo.png" alt="WIX STUDIO" className="nav-logo" /> */}
-        <span className="nav-title">WIX <strong>STUDIO</strong></span>
+        <span className="nav-title"><strong>WIX </strong>STUDIO</span>
       </div>
 
       <div className="nav-center">
-        <RiComputerLine className="icon" />
-        <IoTabletPortraitOutline className="icon" />
-        <LiaMobileSolid className="icon" />
+        <button
+          className={`icon-btn ${active === 'desktop' ? 'active' : ''}`}
+          onClick={() => setActive('desktop')}
+        >
+          <RiComputerLine />
+        </button>
+
+        <button
+          className={`icon-btn ${active === 'tablet' ? 'active' : ''}`}
+          onClick={() => setActive('tablet')}
+        >
+          <IoTabletPortraitOutline />
+        </button>
+
+        <button
+          className={`icon-btn ${active === 'mobile' ? 'active' : ''}`}
+          onClick={() => setActive('mobile')}
+        >
+          <LiaMobileSolid />
+        </button>
       </div>
 
-      <div className="nav-right">
-        <button className="edit-btn">Edit Template</button>
-      </div>
+      <a href='/' className="nav-right">
+        <div className="edit-btn">
+          Edit Template
+          <IoMdReturnRight className="arrow-icon" />
+        </div>
+      </a>
     </header>
   );
 }
