27f2cc658943540dc2d9757c1603bc72fdbcd7b2
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-07-04 23:02:58 +0530

    modified:   Projects/ClassProject/package-lock.json 	modified:   Projects/ClassProject/package.json 	modified:   Projects/ClassProject/src/App.js 	modified:   Projects/ClassProject/src/index.css

--- Projects/ClassProject/src/App.js
+++ Projects/ClassProject/src/App.js
@@ -6,9 +6,22 @@ import LogosCarousel from './components/LogosCarousel';
 import Testimonials from './components/Testimonials';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import "./index.css";
 
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,  // animation duration
+      once: true,     // animate only once
+      offset: 120,    // trigger when 120px above
+    });
+  }, []);
+
+  
   return (
     <>
       <Navbar />
